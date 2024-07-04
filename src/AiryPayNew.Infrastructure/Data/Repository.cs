333de1d50df262ce1db42082ef0ab2d20e8e743e
oqo0 <oq.roman@gmail.com>
2024-07-04 09:58:22 +0300

    Create universal repository

---
+++ src/AiryPayNew.Infrastructure/Data/Repository.cs
@@ -0,0 +1,21 @@
+﻿using AiryPayNew.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace AiryPayNew.Infrastructure.Data;
+
+public abstract class Repository<TId, TEntity>(ApplicationDbContext dbContext) : IRepository<TId, TEntity>
+    where TId : IId
+    where TEntity : class, IEntity<TId>
+{
+    public async Task<TEntity?> GetByIdAsync(TId id)
+    {
+        return await dbContext.Set<TEntity>().FirstOrDefaultAsync(e => e.Id.Equals(id));
+    }
+
+    public async Task<TId> Create(TEntity data)
+    {
+        dbContext.Set<TEntity>().Add(data);
+        await dbContext.SaveChangesAsync();
+        return data.Id;
+    }
+}
