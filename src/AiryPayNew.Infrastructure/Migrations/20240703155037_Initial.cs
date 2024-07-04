3bf6709340d35d4b8172704fe4a57bccda0d625b
oqo0 <oq.roman@gmail.com>
2024-07-04 08:24:33 +0300

    Add migrations

---
+++ src/AiryPayNew.Infrastructure/Migrations/20240703155037_Initial.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace AiryPayNew.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "shops",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Balance = table.Column<decimal>(type: "numeric", nullable: false),
+                    Blocked = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_shops", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "products",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Emoji = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false),
+                    ShopId = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_products_shops_ShopId",
+                        column: x => x.ShopId,
+                        principalTable: "shops",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "withdrawals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    WithdrawalStatus = table.Column<int>(type: "integer", nullable: false),
+                    Way = table.Column<string>(type: "text", nullable: false),
+                    DateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ShopId = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_withdrawals", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_withdrawals_shops_ShopId",
+                        column: x => x.ShopId,
+                        principalTable: "shops",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "bills",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    bill_secret = table.Column<string>(type: "text", nullable: false),
+                    BillStatus = table.Column<int>(type: "integer", nullable: false),
+                    ShopId = table.Column<long>(type: "bigint", nullable: true),
+                    ProductId = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_bills", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_bills_products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "products",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_bills_shops_ShopId",
+                        column: x => x.ShopId,
+                        principalTable: "shops",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "purchases",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    BillId = table.Column<int>(type: "integer", nullable: true),
+                    ShopId = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_purchases", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_purchases_bills_BillId",
+                        column: x => x.BillId,
+                        principalTable: "bills",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_purchases_shops_ShopId",
+                        column: x => x.ShopId,
+                        principalTable: "shops",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_bills_ProductId",
+                table: "bills",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_bills_ShopId",
+                table: "bills",
+                column: "ShopId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_products_ShopId",
+                table: "products",
+                column: "ShopId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_purchases_BillId",
+                table: "purchases",
+                column: "BillId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_purchases_ShopId",
+                table: "purchases",
+                column: "ShopId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_withdrawals_ShopId",
+                table: "withdrawals",
+                column: "ShopId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "purchases");
+
+            migrationBuilder.DropTable(
+                name: "withdrawals");
+
+            migrationBuilder.DropTable(
+                name: "bills");
+
+            migrationBuilder.DropTable(
+                name: "products");
+
+            migrationBuilder.DropTable(
+                name: "shops");
+        }
+    }
+}
