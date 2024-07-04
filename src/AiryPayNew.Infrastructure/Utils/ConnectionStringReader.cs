8bcd14be84971d2f30124ced8fc1505f708960e3
oqo0 <oq.roman@gmail.com>
2024-07-04 08:23:48 +0300

    Implement database connection string generator

---
+++ src/AiryPayNew.Infrastructure/Utils/ConnectionStringReader.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+namespace AiryPayNew.Infrastructure.Utils;
+
+internal record DatabaseCredential(string ConnectionStringName, string EnvironmentVariableName);
+
+internal static class ConnectionStringReader
+{
+    private static readonly IList<DatabaseCredential> DatabaseCredentials =
+    [
+        new DatabaseCredential("Host", "POSTGRES_DB"),
+        new DatabaseCredential("Port", "POSTGRES_USER"),
+        new DatabaseCredential("Database", "POSTGRES_PASSWORD"),
+        new DatabaseCredential("Userid", "POSTGRES_HOST"),
+        new DatabaseCredential("Pwd", "POSTGRES_PORT"),
+    ];
+    
+    public static string GetString()
+    {
+        var stringBuilder = new StringBuilder();
+        
+        foreach (var credential in DatabaseCredentials)
+        {
+            stringBuilder.Append(
+                GetConnectionStringElement(
+                    credential.ConnectionStringName,
+                    credential.EnvironmentVariableName));
+        }
+
+        stringBuilder.Append("Pooling=true;");
+
+        return stringBuilder.ToString();
+    }
+
+    private static string GetConnectionStringElement(string connectionStringParameterName, string connectionStringValue)
+    {
+        return $"{connectionStringParameterName}={GetDatabaseCredential(connectionStringValue)};";
+    }
+    
+    private static string GetDatabaseCredential(string environmentVariable)
+    {
+        var data = Environment.GetEnvironmentVariable(environmentVariable);
+        if (data is null)
+            throw new InvalidOperationException($"Environment variable {environmentVariable} is missing");
+
+        return data;
+    }
+}
