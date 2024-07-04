c3989fe199b02c67651008dd338846d08ed8ab2e
oqo0 <oq.roman@gmail.com>
2024-07-04 10:58:40 +0300

    Add mediatr

--- src/AiryPayNew.Application/DependencyInjection.cs
+++ src/AiryPayNew.Application/DependencyInjection.cs
@@ -1,6 +1,15 @@
-﻿namespace AiryPayNew.Application;
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
 
-public class DependencyInjection
+namespace AiryPayNew.Application;
+
+public static class DependencyInjection
 {
-    
+    public static IServiceCollection AddApplication(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddMediatR(c =>
+            c.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        
+        return serviceCollection;
+    }
 }
