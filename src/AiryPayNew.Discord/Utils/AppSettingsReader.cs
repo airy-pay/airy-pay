93e78139427b66c03029f99be4138bce9b7b3044
oqo0 <oq.roman@gmail.com>
2024-07-05 18:42:39 +0300

    Implement basic app settings reader

---
+++ src/AiryPayNew.Discord/Utils/AppSettingsReader.cs
@@ -0,0 +1,16 @@
+﻿using AiryPayNew.Discord.Settings;
+using Microsoft.Extensions.Configuration;
+
+namespace AiryPayNew.Discord.Utils;
+
+public static class AppSettingsReader
+{
+    public static AppSettings GetSettings(IConfiguration configuration)
+    {
+        var appSettings = configuration.Get<AppSettings>();
+        if (appSettings is null)
+            throw new InvalidDataException("Invalid data in app settings");
+
+        return appSettings;
+    }
+}
