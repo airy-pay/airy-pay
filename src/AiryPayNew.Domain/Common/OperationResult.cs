fbb98e94e77d307ced90053e8bf0f69170f202d3
oqo0 <oq.roman@gmail.com>
2024-07-06 13:27:44 +0300

    Add non generic version

--- src/AiryPayNew.Domain/Common/OperationResult.cs
+++ src/AiryPayNew.Domain/Common/OperationResult.cs
@@ -1,23 +1,39 @@
 ﻿namespace AiryPayNew.Domain.Common;
 
-public class OperationResult<T>
+public class OperationResult<T> : OperationResult
 {
-    public bool Successful { get; set; }
-    public string? ErrorMessage { get; set; }
     public T Entity { get; set; }
     
-    public OperationResult(bool successful, T entity)
+    public OperationResult(bool successful, T entity) : base(successful)
     {
-        Successful = successful;
         Entity = entity;
     }
     
-    public OperationResult(bool successful, string errorMessage, T entity)
+    public OperationResult(bool successful, string errorMessage, T entity) : base(successful, errorMessage)
     {
-        Successful = successful;
-        ErrorMessage = errorMessage;
         Entity = entity;
     }
 
     public static OperationResult<T> Success(T entity) => new(true, entity);
+    public static OperationResult<T> Error(T entity, string errorMessage) => new(false, errorMessage, entity);
+}
+
+public class OperationResult
+{
+    public bool Successful { get; set; }
+    public string? ErrorMessage { get; set; }
+    
+    public OperationResult(bool successful)
+    {
+        Successful = successful;
+    }
+    
+    public OperationResult(bool successful, string errorMessage)
+    {
+        Successful = successful;
+        ErrorMessage = errorMessage;
+    }
+
+    public static OperationResult Success() => new(true);
+    public static OperationResult Error(string errorMessage) => new(false, errorMessage);
 }
