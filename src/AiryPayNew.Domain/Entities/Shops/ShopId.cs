acd37ea11dc6055acd70962317f99ca0e2f9a195
oqo0 <oq.roman@gmail.com>
2024-07-06 09:12:19 +0300

    Allow `ulong as parameter`

--- src/AiryPayNew.Domain/Entities/Shops/ShopId.cs
+++ src/AiryPayNew.Domain/Entities/Shops/ShopId.cs
@@ -5,4 +5,6 @@ namespace AiryPayNew.Domain.Entities.Shops;
 public record struct ShopId(long Value) : IIdBase<long>
 {
     public static IId Default() => new ShopId(1);
+
+    public ShopId(ulong Value) : this((long) Value) { }
 }
