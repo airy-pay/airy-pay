981ffc9f2278ddd9d4e04578c6d1ec3ddc29879d
oqo0 <oq.roman@gmail.com>
2024-07-14 19:33:53 +0300

    Add Serilog #2

--- src/AiryPayNew.Presentation/Program.cs
+++ src/AiryPayNew.Presentation/Program.cs
@@ -2,20 +2,41 @@
 using AiryPayNew.Discord.Http;
 using AiryPayNew.Discord.Utils;
 using AiryPayNew.Shared.Utils;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
 
-builder.Configuration.AddYamlFile("paymentsettings.yaml");
-var appSettings = builder.Configuration.GetAppSettings();
-
-LanguageChanger.Update(appSettings.Language);
-
-builder.Services
-    .AddDiscordHost(appSettings)
-    .AddServices(appSettings);
-
-var app = builder.Build();
-
-app.AddHttpEndpoints();
-
-app.Run();
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+    
+    builder.Services.AddSerilog((services, lc) => lc
+        .ReadFrom.Configuration(builder.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext());
+    
+    builder.Configuration.AddYamlFile("paymentsettings.yaml");
+    var appSettings = builder.Configuration.GetAppSettings();
+    
+    LanguageChanger.Update(appSettings.Language);
+    
+    builder.Services
+        .AddDiscordHost(appSettings)
+        .AddServices(appSettings);
+    
+    var app = builder.Build();
+    
+    app.AddHttpEndpoints();
+    
+    await app.RunAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
