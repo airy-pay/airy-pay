a2bcf7cbe2ea6ad504229eae6d7f4736652de4f4
oqo0 <oq.roman@gmail.com>
2024-07-15 17:05:37 +0300

    Add migrations

--- src/AiryPayNew.Infrastructure/Migrations/20240715131911_AddCommission.Designer.cs
+++ src/AiryPayNew.Infrastructure/Migrations/20240715131911_AddCommission.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AiryPayNew.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace AiryPayNew.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240715131911_AddCommission")]
+    partial class AddCommission
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
