eace224a2dc4b4cfafcff256af16b98ca3be92d7
oqo0 <oq.roman@gmail.com>
2025-09-19 19:05:54 +0300

    Fix invalid settings

--- src/AiryPay.Shared/Settings/AppSettingsNested/PaymentSystemsSettings/RuKassaSettings.cs
+++ src/AiryPay.Shared/Settings/AppSettingsNested/PaymentSystemsSettings/RuKassaSettings.cs
@@ -4,15 +4,15 @@ namespace AiryPay.Shared.Settings.AppSettingsNested.PaymentSystemsSettings;
 
 public class RuKassaSettings
 {
-    [YamlMember(typeof(RuKassaSettings), Alias = "merchantId")]
+    [YamlMember(typeof(int), Alias = "merchantId")]
     public required int MerchantId { get; set; }
     
-    [YamlMember(typeof(RuKassaSettings), Alias = "token")]
+    [YamlMember(typeof(string), Alias = "token")]
     public required string Token { get; set; }
     
-    [YamlMember(typeof(RuKassaSettings), Alias = "userEmail")]
+    [YamlMember(typeof(string), Alias = "userEmail")]
     public required string UserEmail { get; set; }
     
-    [YamlMember(typeof(RuKassaSettings), Alias = "userPassword")]
+    [YamlMember(typeof(string), Alias = "userPassword")]
     public required string UserPassword { get; set; }
 }
